819de63c8b7e4a275a857b5f96334bbb511a748e
Zeyad-Magdy <zyadmagdy98@gmail.com>
2020-08-23 14:15:07 +0200

    using cross-validation

--- test.py
+++ test.py
@@ -20,6 +20,15 @@ X_train_scaled = preprocessing.scale(X_train)
 scaler = preprocessing.StandardScaler().fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 X_test_scaled = scaler.transform(X_test)
-print (X_test_scaled.mean(axis=0))
-print (X_test_scaled.std(axis=0))
+# print (X_test_scaled.mean(axis=0))
+# print (X_test_scaled.std(axis=0))
 pipeline = make_pipeline(preprocessing.StandardScaler(), RandomForestRegressor(n_estimators=100))
+
+# print (pipeline.get_params())
+hyperparameters = { 'randomforestregressor__max_features' : ['auto', 'sqrt', 'log2'],
+                  'randomforestregressor__max_depth': [None, 5, 3, 1]}
+clf = GridSearchCV(pipeline, hyperparameters, cv=10)
+clf.fit(X_train, y_train)
+# print (clf.best_params_)
+
+print (clf.refit)
