645c9af45cfd6ceeb611d8ebf79b7333069d59eb
Ragy Ragheb <ragyreskandar@yahoo.com>
2020-08-23 13:51:52 +0200

    Preprocessing Implement

--- test.py
+++ test.py
@@ -6,11 +6,20 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.externals import joblib
+import joblib
 
 
-dataset_url = './winequality-red.csv'
-data = pd.read_csv(dataset_url, sep=';')
 
-print(data.head())
+dataset = './winequality-red.csv'
+data = pd.read_csv(dataset, sep=';')
 
+y = data.quality
+X = data.drop('quality', axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123, stratify=y)
+X_train_scaled = preprocessing.scale(X_train)
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+print (X_test_scaled.mean(axis=0))
+print (X_test_scaled.std(axis=0))
+pipeline = make_pipeline(preprocessing.StandardScaler(), RandomForestRegressor(n_estimators=100))
